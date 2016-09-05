8061c816044891464af7042d72b5ed05fd86b8f1
Sharif Malik <sharif.malik@neptune-int.com>
2016-09-05 09:27:59 +0530

    Added web creating server example5.js

---
+++ node/examples/example5.js
@@ -0,0 +1,36 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+//Create the Server
+http.createServer(function (request, response){
+
+	//Parse the request containing filename
+	var pathname = url.parse(request.url).pathname;	
+	
+	// Print the name of the file for which request is made.
+	console.log("request for "+ pathname + " received");
+
+	// Read the requested file content from file system
+	fs.readFile(pathname.substr(1), function (err, data) {
+		
+		if(err){
+			console.log(err);
+
+			// HTTP Status: 404 : NOT FOUND
+			response.writeHead(404, {'Content-Type': 'text/html'});		
+
+		} else {
+			//HTTP status : 200 : OK
+			response.writeHead(200, {'Content-Type': 'text/html'});
+			response.write(data.toString());
+		}
+		
+		//send response body
+		response.end();	
+	});
+
+
+}).listen(1991);
+
+console.log("Server is running at http://localhost:1991");
