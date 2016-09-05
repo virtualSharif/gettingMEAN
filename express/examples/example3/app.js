11b19f270d9c4162aa6576e00948e7129105d1ef
Sharif Malik <sharif.malik@neptune-int.com>
2016-09-05 23:26:50 +0530

    added prefix to all the APIS

--- express/examples/example3/app.js
+++ express/examples/example3/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var controller = require('./controllers');
 var app = express();
 
-app.use('/', controller);
+app.use('/api', controller);
 
 app.listen(1991, function() {
 	console.log("Application is listening at http://localhost:1991");
