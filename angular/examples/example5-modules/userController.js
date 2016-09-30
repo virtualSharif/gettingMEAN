b577ca42ea09403478b94036b9aabec216c971db
Sharif Malik <sharif.malik@neptune-int.com>
2016-09-30 23:58:25 +0530

    added example5

---
+++ angular/examples/example5-modules/userController.js
@@ -0,0 +1,10 @@
+//Creating controller here  
+myApp.controller('UserController', function($scope) {  
+	
+	//Creating model here
+	$scope.user =  {  
+		'name'    :   'Sharif Malik',  
+		'address' :   '12-13-283/A1, Chinchwad, Pune',  
+		'email'   :   'malik_sharif@ymail.com'
+	};  
+});
